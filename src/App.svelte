9fb61a7ca91270aef6dbd734a1b470416fa16d88
ModdedOutlaw <moddedoutlaw33@gmail.com>
2024-04-20 18:49:20 -0400

    Liftium Interaction

--- src/App.svelte
+++ src/App.svelte
@@ -1,58 +1,177 @@
-
 <script lang="ts">
-    import {onMount} from 'svelte'
-    import {login, logout, restore, session, get_info, upliftium_info} from './wharf'
+  import { onMount } from "svelte";
+  import {
+    login,
+    logout,
+    restore,
+    session,
+    get_info,
+    transfer,
+    transferNFT,
+    get_collection,
+    get_tokens,
 
-    onMount(restore)
+  } from "./wharf";
+  import { assetsStore, tokensStore } from "./wharf";
 
-	export let name: string;
+  onMount(restore);
+  let amount = "1"; // default value for the dropdown
+  let amountInMillions = 1; // default value
+  let receiver = ""; // default value for the text input
+  let account = "";
+
+  let assets = [];
+  let selectedAssetIds = [];
+  let balances = []; // default value
+
+  let currentPage = 1;
+  const itemsPerPage = 10;
+  
+  onMount(() => {
+    const unsubscribe = assetsStore.subscribe((value) => {
+      assets = value;
+    });
+
+    return unsubscribe; // unsubscribe when the component is destroyed
+  });
+
+  onMount(() => {
+    const unsubscribe = tokensStore.subscribe((value) => {
+      balances = value;
+    });
+
+    return unsubscribe; // unsubscribe when the component is destroyed
+  });
+  
+
+  $: paginatedAssets = assets.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+  function toggleSelection(assetId) {
+    if (selectedAssetIds.includes(assetId)) {
+      selectedAssetIds = selectedAssetIds.filter((id) => id !== assetId);
+    } else {
+      selectedAssetIds = [...selectedAssetIds, assetId];
+    }
+    console.log(selectedAssetIds);
+  }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>WAX</h1>
+  <p>Using Wharfkit !</p>
 </main>
-<div>
-	{#if $session}
-		<button on:click={logout}>Logout ({$session.actor})</button>
-	{:else}
-		<button on:click={login}>Login</button>
-	{/if}
-</div>
-<div>
-	{#if $session}
-		<button on:click={() => get_info($session.actor.toString())}>Get Info ({$session.actor})</button>
-	{:else}
-		<div></div>
-	{/if}
-</div>
-<div>
-	{#if $upliftium_info}
-	<img src={'https://atomichub-ipfs.com/ipfs/' + $upliftium_info} alt="description" />
 
-	{:else}
-		<div></div>
-	{/if}
-	
+<div>
+  {#if $session}
+    <button on:click={logout}>Logout ({$session.actor})</button>
+  {:else}
+    <button on:click={login}>Login</button>
+  {/if}
 </div>
+
+<div>
+  {#if $session}
+  
+    <button on:click={() => get_tokens($session.actor.toString())}
+		>Get Token Info ({$session.actor})</button
+	  >
+	  <table>
+		<thead>
+		  <tr>
+			<th>Token</th>
+			<th>Balance</th>
+			<!-- Add more columns if needed -->
+		  </tr>
+		</thead>
+		<tbody>
+		  {#each balances as balance (balance.currency)}
+			<tr>
+			  <td>{balance.currency}</td>
+			  <td>{balance.amount}</td>
+			  <!-- Add more cells if needed -->
+			</tr>
+		  {/each}
+		</tbody>
+	  </table>
+    <label>
+      Amount:
+      <select bind:value={amount}>
+        <option value="0.0001 LIFTIUM">0.0001 LIFTIUM</option>
+        <option value="0.0002 LIFTIUM">0.0002 LIFTIUM</option>
+        <option value="0.0003 LIFTIUM">0.0003 LIFTIUM</option>
+        <!-- add more options as needed -->
+      </select>
+    </label>
+
+    <label>
+      Receiver of LIFTIUM:
+      <input type="text" bind:value={receiver} />
+    </label>
+
+    <button on:click={() => transfer(receiver, amount)}>Transfer LIFTIUM</button>
+	<div>
+		<label>
+			Amount (in millions of LIFTIUM):
+			<input type="number" min="1" step="1" bind:value={amountInMillions} />
+			<button on:click={() => transfer(receiver, (amountInMillions * 1000000).toFixed(4).toString() + " LIFTIUM")}>Transfer LIFTIUM</button>
+		  </label>
+	</div>
+    <div>
+		<label>
+			Receiver of NFT(s):
+			<input type="text" bind:value={account} />
+		  </label>
+		  <button on:click={() => transferNFT(account, selectedAssetIds)}
+			>TransferNFT ({$session.actor})</button
+		  >
+		  <button on:click={() => get_collection($session.actor.toString())}
+			>Get 1 MIO asset IDS)</button
+		  >
+      <button on:click={() => (currentPage = Math.max(1, currentPage - 1))}
+        >Previous</button
+      >
+      <button
+        on:click={() =>
+          (currentPage = Math.min(
+            Math.ceil(assets.length / itemsPerPage),
+            currentPage + 1
+          ))}>Next</button
+      >
+      {#each paginatedAssets as assetId (assetId)}
+        <button
+          class:selected={selectedAssetIds.includes(assetId)}
+          on:click={() => toggleSelection(assetId)}>{assetId}</button
+        >{/each}
+    </div>
+  {:else}
+    <div></div>
+  {/if}
+</div>
+
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+  .selected {
+    background-color: #007bff; /* change to your preferred color */
+    color: white;
+  }
 </style>
--- src/wharf.ts
+++ src/wharf.ts
@@ -1,110 +1,239 @@
-import {Chains, Session, SessionKit} from '@wharfkit/session'
-import {WebRenderer} from '@wharfkit/web-renderer'
-import {writable, type Writable} from 'svelte/store'
-import { WalletPluginCloudWallet } from '@wharfkit/wallet-plugin-cloudwallet'
-import { WalletPluginAnchor } from "@wharfkit/wallet-plugin-anchor"
+import { Chains, Session, SessionKit } from "@wharfkit/session";
+import { WebRenderer } from "@wharfkit/web-renderer";
+import { writable, type Writable } from "svelte/store";
+import { WalletPluginCloudWallet } from "@wharfkit/wallet-plugin-cloudwallet";
+import { WalletPluginAnchor } from "@wharfkit/wallet-plugin-anchor";
+import { TransactPluginResourceProvider } from "@wharfkit/transact-plugin-resource-provider";
+import { APIClient, Transaction, UInt64Type, UInt64 } from "@wharfkit/antelope";
+import { Contract } from "./contract";
+import { ContractKit } from "@wharfkit/contract";
+import { AccountKit } from "@wharfkit/account";
+import Account from "@wharfkit/contract";
 
+//import abi from "./eosio-abi.json" // ABI for the eosio.token contract
 
-import { APIClient } from "@wharfkit/antelope"
-import { Contract } from "@wharfkit/contract"
-import { AccountKit } from "@wharfkit/account"
-import Account  from "@wharfkit/contract"
+interface ResponseData {
+  account: string;
+  assets: string;
+}
 
+interface CollectionData {
+  success: boolean;
+  data: {
+    contract: string;
+    collection_name: string;
+    name: string;
+    img: string;
+    author: string;
+    allow_notify: boolean;
+    authorized_accounts: string[];
+    notify_accounts: string[];
+    market_fee: number;
+    nestedData: {
+      img: string;
+      url: string;
+      name: string;
+      description: string;
+    };
+    created_at_time: string;
+    created_at_block: string;
+  };
+  query_time: number;
+}
 
-const accountKit = new AccountKit(Chains.WAX)
+interface UpliftiumHiData {
+  account: string;
+  templates: {
+    template_id: string;
+    assets: string;
+  }[];
+}
 
-const accountArgs = {
-    data: {
-      account_name: "modded.gm",
-      // All account data should be passed here.
-    },
-    client: new APIClient({ url: "https://api.waxsweden.org/" }),
-  }
-
-const p_account = new Account(accountArgs)
-
-
-console.log(p_account)
+//const accountKit = new AccountKit(Chains.WAX);
 
 const client = new APIClient({
   url: "https://api.waxsweden.org/",
-})
+});
 
 const client2 = new APIClient({
-    url: "https://wax.api.atomicassets.io/",
-  })
+  url: "https://wax.api.atomicassets.io/",
+});
 
-const wcw = new WalletPluginCloudWallet()
-const anchor = new WalletPluginAnchor()
+const clientWaxLight = new APIClient({
+  url: "https://wax.light-api.net/",
+});
+const wcw = new WalletPluginCloudWallet();
+const anchor = new WalletPluginAnchor();
 // The blockchain(s) this application is deployed on
 
-const chains = [Chains.WAX]
+const chains = [Chains.WAX];
+
+const contract = new Contract({ client: client });
 
 // Create a new session kit instance
-export const sessionKit = new SessionKit(
-    {
-        appName: 'wallet-login-example',
-        chains,
-        ui: new WebRenderer(),
-        walletPlugins: [wcw,anchor],
-      
-    }
-)
+export const sessionKit = new SessionKit({
+  appName: "wallet-login-example",
+  chains,
+  ui: new WebRenderer(),
+  walletPlugins: [wcw, anchor],
+});
 
 // Storage for the current user session
-export let session: Writable<Session | undefined> = writable()
-export let upliftium_info: Writable<Session | undefined> = writable(null);
+export let session: Writable<Session | undefined> = writable();
+export let upliftium_image: Writable<Session | string> = writable();
+export let upliftium_hi_data: Writable<Session | UpliftiumHiData> = writable();
+export let assetsStore = writable([]);
+export let tokensStore = writable([]);
 
+export async function transfer(name: string, quantity: string) {
+  let sessionValue: Session | undefined;
+  session.subscribe((value) => (sessionValue = value));
+  console.log(sessionValue.permissionLevel);
+  const transactionArguments = {
+    action: {
+      account: "tokenizednft",
+      name: "transfer",
+      authorization: [sessionValue.permissionLevel],
+      data: {
+        from: sessionValue.permissionLevel.actor.toString(),
+        to: name,
+        quantity: quantity,
+        memo: "LIFTIUM Transfer",
+      },
+    },
+  };
+  if (sessionValue) {
+    let response = await sessionValue.transact(transactionArguments);
+    console.log(response.response);
+    console.log("STATUS = " + response.response.processed.receipt.status);
+    console.log("TX_id = " + response.response.transaction_id);
+  } else {
+    console.error("No session available");
+  }
+}
+
+export async function transferNFT(name: string, assetID: UInt64Type) {
+  let sessionValue: Session | undefined;
+  session.subscribe((value) => (sessionValue = value));
+  console.log(sessionValue.permissionLevel);
+  const transactionArguments = {
+    action: {
+      account: "atomicassets",
+      name: "transfer",
+      authorization: [sessionValue.permissionLevel],
+      data: {
+        from: sessionValue.permissionLevel.actor.toString(),
+        to: name,
+        asset_ids: assetID,
+        memo: "Upliftium NFT transfer",
+      },
+    },
+  };
+  if (sessionValue) {
+    let response = await sessionValue.transact(transactionArguments);
+    console.log(response.response);
+    console.log("STATUS = " + response.response.processed.receipt.status);
+    console.log("TX_id = " + response.response.transaction_id);
+  } else {
+    console.error("No session available");
+  }
+}
 
 // A function that performs the login and sets the session variable
 
 export async function login() {
-    
-    const response = await sessionKit.login()
-    console.log(response);
-    if (response.session) {
-        session.set(response.session)
-    }
-    window.location.reload();
+  const response = await sessionKit.login();
+  console.log(response);
+  if (response.session) {
+    session.set(response.session);
+  }
+  window.location.reload();
 }
 // A function that performs the logout and clears the session variable
 export async function logout() {
-    await sessionKit.logout()
-    session.set(undefined)
-    upliftium_info.set(undefined)
+  await sessionKit.logout();
+  session.set(undefined);
+  upliftium_image.set(undefined);
+  localStorage.clear();
 }
 // A function that performs the restore and sets the session variable
 export async function restore() {
-    session.set(await sessionKit.restore())
-   // upliftium_info.set(await sessionKit.restore())
+  session.set(await sessionKit.restore());
+  const img = localStorage.getItem("upliftium_img");
+  upliftium_image.set(img);
+}
+
+export async function get_collection(name: string) {
+  const upliftium_collection_response = await client2.call({
+    path: "/atomicassets/v1/accounts/" + name + "/upliftium.hi",
+  });
+
+  const owner_collection_response = await client2.call({
+    path:
+      "/atomicassets/v1/assets/?collection_name=upliftium.hi&owner=" +
+      name +
+      "&page=1&limit=1000&order=desc&sort=asset_id",
+  });
+  //wax.api.atomicassets.io/atomicassets/v1/assets?collection_name=upliftium.hi&owner=modded.gm&page=1&limit=500&order=desc&sort=asset_id
+
+  //const responseData = upliftium_collection_response as { data: ResponseData[] };
+
+  console.log(upliftium_collection_response);
+
+  console.log(owner_collection_response);
+
+  const collectionData = upliftium_collection_response as {
+    data: UpliftiumHiData;
+  };
+
+  console.log(collectionData.data.templates);
+  // Assuming response is your response object array
+  const assetIds = owner_collection_response.data.map((item) => item.asset_id);
+  console.log(assetIds);
+  assetsStore.set(assetIds); // update the store with the first 20 assets
 }
 
 export async function get_info(name: string) {
-    //const name = await client.v1.chain.get_account("modded.gm".toString())
-    // https://wax.api.atomicassets.io/atomicassets/v1/accounts?owner=modded.gm
-    //https://wax.api.atomicassets.io/atomicassets/v1/collections/upliftium.hi
+  const response = await client2.call({
+    path: "/atomicassets/v1/accounts" + "?owner=" + name,
+  });
 
-    const account = await accountKit.load("modded.gm")
+  const upliftium_collection_response = await client2.call({
+    path: "/atomicassets/v1/collections/upliftium.hi",
+  });
 
-    const response = await client2.call({
-        path: "/atomicassets/v1/accounts"+ "?owner=" +name,
-    
-      })
+  const responseData = response as { data: ResponseData[] };
 
-      const upliftium_collection_info = await client2.call({
-        path: "/atomicassets/v1/collections/upliftium.hi",
-    
-      })
- 
-    console.log(name)
-    const balance = await account.balance()
+  console.log(
+    "ACCOUNT " +
+      responseData.data[0].account +
+      " HAS " +
+      responseData.data[0].assets +
+      " ASSETS!"
+  );
 
-    console.log(balance.toJSON())
-    //console.log("ACCOUNT " + response.data[0].account+" HAS "+response.data[0].assets+" ASSETS!")
-    //console.log("Upliftium Image Id: "+upliftium_collection_info.data.img)
-    //upliftium_info.set(upliftium_collection_info.data.img);
-    console.log("CHECK =" + upliftium_info);
-    console.log(response)
-    
-   }
-    
+  const collectionData = upliftium_collection_response as {
+    data: CollectionData;
+  };
+  console.log(collectionData.data.data.img);
+  upliftium_image.set(collectionData.data.data.img);
+  localStorage.setItem("upliftium_img", collectionData.data.data.img);
+}
+
+//'https://wax.blokcrafters.io/v2/state/get_tokens?account=modded.gm'
+//api/topholders/CHAIN/CONTRACT/TOKEN/NUM
+export async function get_tokens(name: string) {
+  const response = await clientWaxLight.call({
+    path: "/api/balances/wax/" + name,
+  });
+
+  const response2 = await clientWaxLight.call({
+    path: "/api/topholders/wax/tokenizednft/LIFTIUM/10",
+  });
+
+  const responseData = response as { data: ResponseData[] };
+
+  console.log(responseData.balances);
+  console.log(response2);
+  tokensStore.set(responseData.balances);
+}
